23946b4fd31576204b7b29dc97470d8f0b99b788
Guanglei BAO <guanglei@amazon.com>
2015-08-06 14:24:33 +1000

    Adding decodeAuthMessage.sh

--- decodeAuthMessage.sh
+++ decodeAuthMessage.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Needs sts:DecodeAuthorizationMessage privilege to execute.
+# No production guarantee.
 aws sts decode-authorization-message --encoded-message ${1} --query 'DecodedMessage' | sed -e 's/\\"/"/g' -e 's/"{/{/g' -e 's/}"/}/g' | python -m json.tool
